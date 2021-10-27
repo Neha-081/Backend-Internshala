b764c8f89bf4200790ff0d3cd0254f5c82d3da4f
Neha <nehaprajapati1150@gmail.com>
2021-10-27 13:15:47 +0530

    src

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -24,11 +24,14 @@ function logout(){
       var input = document.getElementById("oninp").value;
       var input2 = document.getElementById("oninp2").value;
       var input3 = document.getElementById("oninp3").value;
+      var input4= document.getElementById("location").value;
       if(input=="1k"|| input=="2k"|| input=="3k"|| input=="5k"){
           window.open("/users/sorting")
       }else if(input2=="2 months"|| input2=="3 months"|| input2=="4 months"|| input2=="5 month"){
         window.open("/users/sorting")
       }else if(input3=="2"|| input3=="3"|| input3=="4"|| input3=="5"){
+        window.open("/users/sorting")
+      }else if(input4=="pune"|| input4=="delhi"|| input4=="banagalore"|| input4=="gurgaon"){
         window.open("/users/sorting")
       }
       
